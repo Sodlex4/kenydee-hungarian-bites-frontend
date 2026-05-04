d578a08ab8a7b237bb8319d7934c6cbd7dc49ac8
sodlex4 <stephenodongo532@gmail.com>
2026-05-04 23:37:33 +0300

    Remove duplicate privacy and terms routes, keep /legal/* versions

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import TermsPage from "./pages/legal/Terms";
@@ -23,8 +21,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/legal/terms" element={<TermsPage />} />
           <Route path="/legal/privacy" element={<PrivacyPage />} />
