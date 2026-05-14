eec602548b46bb57c3218102244c63c115c6f775
sodlex4 <stephenodongo532@gmail.com>
2026-05-14 21:13:03 +0300

    fix: add basename to BrowserRouter for GitHub Pages subpath routing

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <AuthProvider>
         <AdminProfileProvider>
           <Routes>
